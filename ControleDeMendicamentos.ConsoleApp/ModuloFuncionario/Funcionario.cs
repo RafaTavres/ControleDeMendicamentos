bb60c84182d94f15544d78695d081159ea8b9aa5
Rafael S Tavares <ragbriros@gmail.com>
2023-04-25 14:38:46 -0300

    Varias correções de bugs e melhoria nos construtores

--- ControleDeMendicamentos.ConsoleApp/ModuloFuncionario/Funcionario.cs
+++ ControleDeMendicamentos.ConsoleApp/ModuloFuncionario/Funcionario.cs
@@ -11,10 +11,6 @@ namespace ControleDeMendicamentos.ConsoleApp.ModuloFuncionario
         public string nome { get; set; }
         public string CPF { get; set; }
         public string telefone { get; set; }
-        #region maluquice
-        //public string login { get; set; }
-        //public string senha { get; set; }
-        #endregion
         public override void Atualizar(EntidadeBase entidadeAtualizada)
         {
             Funcionario funcionario = (Funcionario)entidadeAtualizada;
